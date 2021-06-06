1272a5ca568a03a8d45aaf283cfc6b83996d27b7
stevenwmorgan <49211170+stevenwmorgan@users.noreply.github.com>
2021-06-06 16:41:50 -0400

    Create Play_game.py
    Code chooses random numbers between 0 and 1, when the sum of these numbers exceeds 1 the programs quits.

---
+++ Play_game.py
@@ -0,0 +1,9 @@
+From numpy import random
+def play_game(): #Play the game
+  s,c = 0.0,0
+  print("Your numbers are:")
+  while s <= 1:
+    x = random.uniform(0,1)
+    s += x
+    c += 1
+  Print("Your game lasted " + str(c) + "rounds")
